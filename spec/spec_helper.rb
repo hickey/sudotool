a3dfda60dfafe431d74fbc15bae72787584d90ff
Gerard Hickey <gerard.hickey@audiencescience.com>
2015-02-01 05:11:16 -0800

    Attempt to support require_relative for 1.8.7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+# Provide basic require_relative support for 1.8.7 builds
+unless Kernel.respond_to?(:require_relative)
+  module Kernel
+    def require_relative(path)
+      require File.join(File.dirname(caller[0]), path.to_str)
+    end
+  end
+end
+
 require_relative '../lib/sudotool'
 require_relative '../lib/sudoerfile'
 require_relative '../lib/sudoright'
